94957845d397a44ffa3520a4607bd989858f2a01
Denis Moreira <silvad281@gmail.com>
2025-05-05 20:09:09 -0300

    feat: add pets

---
+++ src/pages/home/pets.ts
@@ -0,0 +1,38 @@
+export const pets = [
+   {
+      name: "Luna",
+      imageUrl:
+         "https://www.petz.com.br/blog/wp-content/uploads/2023/07/story-1-1.png",
+      bgColor: "bg-green-400",
+   },
+   {
+      name: "Thor",
+      imageUrl:
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH4ks0b8ssy2rBm_jAA6f9rTwtRqZYEBV3JA&s",
+      bgColor: "bg-cyan-300",
+   },
+   {
+      name: "Mel",
+      imageUrl:
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7wwxL6CNQdR51_7IJXxa9S3lyz3WR4K9ZeA&s",
+      bgColor: "bg-orange-400",
+   },
+   {
+      name: "Tigrinho",
+      imageUrl:
+         "https://s2.glbimg.com/yrFgwzwrwxxpO7pJoejZxwxpRx0=/620x620/smart/e.glbimg.com/og/ed/f/original/2022/03/11/mister_mainer.jpeg",
+      bgColor: "bg-purple-300",
+   },
+   {
+      name: "Nane",
+      imageUrl:
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZaVv0R3tZtdPIf37UoJcMYqVvNVwKdgkeACYrnSNfLsO9bELzccyrnhM&s",
+      bgColor: "bg-pink-300",
+   },
+   {
+      name: "Lets",
+      imageUrl:
+         "https://wl-incrivel.cf.tsp.li/resize/728x/jpg/18d/d41/c7dc5e5b5aab5d73278b083260.jpg",
+      bgColor: "bg-pink-300",
+   },
+];
