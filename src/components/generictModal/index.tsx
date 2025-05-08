c0da10af8113d113a2e7c5817e04db045108771f
Denis Moreira <silvad281@gmail.com>
2025-05-07 21:45:52 -0300

    feat: criando componente de modal

---
+++ src/components/generictModal/index.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import styles from "./styles.module.css";
+import { IoIosCloseCircle } from "react-icons/io";
+
+interface ModalProps {
+   isOpen: boolean;
+   onClose: () => void;
+   children: React.ReactNode;
+}
+
+export function Modal({ isOpen, onClose, children }: ModalProps) {
+   const [visible, setVisible] = useState(isOpen);
+
+   useEffect(() => {
+      if (isOpen) {
+         setVisible(true);
+      } else {
+         const timer = setTimeout(() => {
+            setVisible(false);
+         }, 500);
+         return () => clearTimeout(timer);
+      }
+   }, [isOpen]);
+
+   return (
+      visible && (
+         <div
+            className={`${styles.overlay} ${
+               !isOpen ? styles.overlayHidden : ""
+            }`}
+         >
+            <div
+               className={`${styles.modal} ${
+                  !isOpen ? styles.modalHidden : ""
+               }`}
+            >
+               {children}
+            </div>
+            <button className={styles.closeButton} onClick={onClose}>
+               <IoIosCloseCircle size={30} color="white" />
+            </button>
+         </div>
+      )
+   );
+}
