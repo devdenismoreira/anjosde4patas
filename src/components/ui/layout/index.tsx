175ad274700b4c365aaba2dc9d3bc07f1c9f2f6a
Denis Moreira <silvad281@gmail.com>
2025-05-08 21:05:50 -0300

    style: ajuste no layout

--- src/components/ui/layout/index.tsx
+++ src/components/ui/layout/index.tsx
@@ -1,13 +1,21 @@
 import { Header } from "../header";
 import { Outlet } from "react-router-dom";
-// import { Footer } from "../footer";
+import { Footer } from "../footer";
 
 export function Layout() {
    return (
-      <>
+      <div
+         style={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+         }}
+      >
          <Header />
-         <Outlet />
-         {/* <Footer /> */}
-      </>
+         <div style={{ height: "100%", flex: 1 }}>
+            <Outlet />
+         </div>
+         <Footer />
+      </div>
    );
 }
