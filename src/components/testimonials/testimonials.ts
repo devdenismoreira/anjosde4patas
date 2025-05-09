af64f4604f470834285121f0e920283a51185090
Denis Moreira <silvad281@gmail.com>
2025-05-08 21:04:00 -0300

    feat: add Testimonials data

---
+++ src/components/testimonials/testimonials.ts
@@ -0,0 +1,24 @@
+import user1 from "../../assets/user1.jpeg";
+import user2 from "../../assets/user2.jpg";
+import user3 from "../../assets/user3.webp";
+
+export const testimonials = [
+   {
+      name: "Alex Lima",
+      role: "Tutor do Nana",
+      text: "A adoção foi fácil, teve toda orientação e acompanhamento da equipe do Adote Petz. A adaptação do Bob foi super boa, estamos muito bem como unha e carne.",
+      image: user1,
+   },
+   {
+      name: "Angélica Lima",
+      role: "Tutora do Luna",
+      text: "O processo foi fácil, nota 10! Os colaboradores deram todas as orientações que eu precisava. O Flocky se deu bem quando chegou, porém o Woody estranhou o Flocky no começo, mas depois se deram bem.",
+      image: user2,
+   },
+   {
+      name: "Jenifer Cristina",
+      role: "Tutora do Garfield",
+      text: "Quando adotei recebi as orientações para a adaptação do pet. Por ser um ambiente novo, o começo da adaptação foi um pouco receosa para o pet, mas depois de alguns dias, ele já estava bem tranquilo.",
+      image: user3,
+   },
+];
