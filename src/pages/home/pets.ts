c48c507f0be21462021960a1dc0916da9758d618
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2025-05-23 11:18:14 -0300

    feat: edit pets

--- src/pages/home/pets.ts
+++ src/pages/home/pets.ts
@@ -1,62 +1,83 @@
+import caramelo from "../../assets/pets/caramelo.jpeg";
+import brutus from "../../assets/pets/brutus.jpeg";
+import dinho from "../../assets/pets/dinho.jpeg";
+import lets from "../../assets/pets/let.jpeg";
+import nana from "../../assets/pets/nana.jpeg";
+import robson from "../../assets/pets/robson.jpeg";
+// import tas from "../../assets/pets/tas.jpeg";
+import tigrinho from "../../assets/pets/tigrinho.jpeg";
+
 export const pets = [
    {
-      nome: "Luna",
-      imagem:
-         "https://www.petz.com.br/blog/wp-content/uploads/2023/07/story-1-1.png",
+      nome: "Caramelo",
+      imagem: caramelo,
       bgColor: "bg-green-400",
       idade: "2 anos",
-      sexo: "Fêmea",
+      sexo: "Macho",
       descricao:
-         "Luna é uma cadelinha brincalhona, super dócil e ama correr atrás de bolinhas. Foi resgatada de um abrigo superlotado e está pronta para receber muito amor em um novo lar!",
+         "Caramelo é um cachorrinho brincalhão, super dócil e ama correr atrás de bolinhas. Foi resgatado de um abrigo superlotado e está pronto para receber muito amor em um novo lar!",
    },
    {
-      nome: "Thor",
-      imagem:
-         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH4ks0b8ssy2rBm_jAA6f9rTwtRqZYEBV3JA&s",
+      nome: "Brutus",
+      imagem: brutus,
       bgColor: "bg-cyan-300",
       idade: "3 anos",
       sexo: "Macho",
       descricao:
-         "Thor é um verdadeiro guerreiro — forte, carinhoso e protetor. Perfeito para famílias que procuram um companheiro fiel e cheio de energia.",
+         "Brutus é um verdadeiro guerreiro — forte, carinhoso e protetor. Perfeito para famílias que procuram um companheiro fiel e cheio de energia.",
    },
    {
-      nome: "Mel",
-      imagem:
-         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7wwxL6CNQdR51_7IJXxa9S3lyz3WR4K9ZeA&s",
+      nome: "Dinho",
+      imagem: dinho,
       bgColor: "bg-orange-400",
       idade: "1 ano",
       sexo: "Fêmea",
       descricao:
-         "Mel tem esse nome porque é um doce! Adora colo, é muito sociável com outros animais e tem um olhar que derrete qualquer coração.",
+         "Dinho é um gatinho esperto e curioso. Adora brincar com bolinhas de papel e tirar longas sonecas ao sol. Está vacinado e cheio de ronronadas para oferecer.",
    },
    {
-      nome: "Tigrinho",
-      imagem:
-         "https://s2.glbimg.com/yrFgwzwrwxxpO7pJoejZxwxpRx0=/620x620/smart/e.glbimg.com/og/ed/f/original/2022/03/11/mister_mainer.jpeg",
+      nome: "Lets",
+      imagem: lets,
       bgColor: "bg-purple-300",
-      idade: "6 meses",
-      sexo: "Macho",
+      idade: "1 ano",
+      sexo: "Fêmea",
       descricao:
-         "Tigrinho é um gatinho esperto e curioso. Adora brincar com bolinhas de papel e tirar longas sonecas ao sol. Está vacinado e cheio de ronronadas para oferecer.",
+         "Lets é cheia de energia, curiosidade e travessuras. Ainda é filhote, então está aprendendo o mundo com olhos brilhantes e muito entusiasmo!",
    },
    {
       nome: "Nane",
-      imagem:
-         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZaVv0R3tZtdPIf37UoJcMYqVvNVwKdgkeACYrnSNfLsO9bELzccyrnhM&s",
+      imagem: nana,
       bgColor: "bg-pink-300",
-      idade: "4 anos",
+      idade: "2 anos",
       sexo: "Fêmea",
       descricao:
          "Nane é calma, elegante e muito carinhosa. Ideal para quem procura uma companhia tranquila e fiel. Foi resgatada de uma situação de maus-tratos e merece um recomeço cheio de carinho.",
    },
    {
-      nome: "Lets",
-      imagem:
-         "https://wl-incrivel.cf.tsp.li/resize/728x/jpg/18d/d41/c7dc5e5b5aab5d73278b083260.jpg",
+      nome: "Robson",
+      imagem: robson,
       bgColor: "bg-pink-300",
-      idade: "8 meses",
-      sexo: "Fêmea",
+      idade: "3 anos",
+      sexo: "Macho",
       descricao:
-         "Lets é cheia de energia, curiosidade e travessuras. Ainda é filhote, então está aprendendo o mundo com olhos brilhantes e muito entusiasmo!",
+         "Cheio de energia e muito brincalhão! Perfeito para famílias ativas.",
    },
+   {
+      nome: "Triginho",
+      imagem: tigrinho,
+      bgColor: "bg-pink-300",
+      idade: "2 anos",
+      sexo: "Macho",
+      descricao:
+         "Triginho tem esse nome porque é um doce! Adora colo, é muito sociável com outros animais e tem um olhar que derrete qualquer coração.",
+   },
+   // {
+   //    nome: "Tas",
+   //    imagem: tas,
+   //    bgColor: "bg-pink-300",
+   //    idade: "2 anos",
+   //    sexo: "Macho",
+   //    descricao:
+   //       "Super dócil, se dá bem com crianças e outros pets. Um amor de companheira.",
+   // },
 ];
